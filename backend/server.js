ae4878ab8f61c2f50b1e068f7f99834e7a53570a
manuelMarkDenver <manuel.markdenver@gmail.com>
2024-04-16 13:43:04 +0800

    pre-jwt setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoutes from "../backend/routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Server is ready'))
+connectDB();
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
